d6c8b556dcefd1ca12fa517dd3f79949b07511a8
Yang <512127058@qq.com>
2021-06-30 03:02:06 +0200

    编译成功但无内容显示

--- Konzepte und Software Design fuer Cyber-Physische Systeme/Assignment 5/task1.c
+++ Konzepte und Software Design fuer Cyber-Physische Systeme/Assignment 5/task1.c
@@ -14,13 +14,24 @@
 #define SIG SIGALRM              // Alarm clock expired
 #define OLD_ACT NULL                // No previous action
 
+
+
+/*------- Handler for timer signal -------*/
+static void handler(){
+    timer_t t;
+    char buffer[80];
+    strftime(buffer,sizeof(buffer),"%Y-%m-%d %H:%M:%S",localtime(&t));
+    printf("SIGALRM appear at:");
+}
+
 int main(){
 
-    /*------- Establish handler for timer signal -------*/
+    /*------- Establish the handler -------*/
     // The sigaction() system call is used to change the action taken by a process on receipt of a specific signal.
-    struct sigaction sa;
-    sa.sa_flags = SA_SIGINFO;
-    if(sigaction(SIG, &sa,OLD_ACT) == -1){
+    struct sigaction new_act;
+    new_act.sa_flags = SA_SIGINFO;
+    new_act.sa_sigaction = handler;
+    if(sigaction(SIG, &new_act,OLD_ACT) == -1){
         perror("sigaction failed");
         exit(1);
     }
@@ -32,11 +43,14 @@ int main(){
     // sig: the signal number
     sigset_t set;       
     int sig;
-    sigaddset(&set, SIG);
+    sigemptyset(&set);          //initialize and empty the set
+    sigaddset(&set, SIG);       //add signal SIG to the set
     if(sigwait(&set,&sig) == -1){
         perror("sigwait failed");
         exit(1);
     }
+    sigdelset(&set,SIG);        //delete signal from the set
+
 
     /*------- Create timer -------*/
     // timer_create() creates a new per-process interval timer.
@@ -49,7 +63,7 @@ int main(){
         exit(1);
     }
 
-    /*------- Start timer -------*/
+    /*------- Set timer -------*/
     // timer_settime() arms or disarms the timer identified by timerid.
     // FLAG:                        specifies a relative timer
     // new_value:                   specifies the new initial value and the new interval for the timer
@@ -57,13 +71,19 @@ int main(){
     // new_value -> it_value:       set the timer to initially expire at the given time
     // new_value -> it_interval:    specified the period of the timer
 
-    struct itimerspec timer1_time;                  // the new_value
-    timer1_time.it_value.tv_sec     = 2;            // 2s
-    timer1_time.it_value.tv_nsec    = 500000000;    // 0.5s -> after 2+0.5=2.5s timer first expire
-    timer1_time.it_interval.tv_sec  = 0;            // 0s
-    timer1_time.it_interval.tv_nsec = 100000000;    // 0.1s -> period of the timer is 0.1s
-    if(timer_settime(timer1,FLAG, &timer1_time, OLD_VALUE) == -1){
+    struct itimerspec timer1_value;                  // the new_value
+    timer1_value.it_value.tv_sec     = 2;            // 2s
+    timer1_value.it_value.tv_nsec    = 500000000;    // 0.5s -> after 2+0.5=2.5s timer first expire
+    timer1_value.it_interval.tv_sec  = 0;            // 0s
+    timer1_value.it_interval.tv_nsec = 100000000;    // 0.1s -> period of the timer is 0.1s
+    if(timer_settime(timer1,FLAG, &timer1_value, OLD_VALUE) == -1){
         perror("timer_settime failed");
         exit(1);
     }
+
+    while(1){
+        printf("--------Here is the main loop--------");
+        sleep(1);
+    }
+
 }
