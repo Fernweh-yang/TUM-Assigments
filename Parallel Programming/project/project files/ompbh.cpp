96b3da642bfc15acdbd288a437b708fd76268064
Yang <512127058@qq.com>
2021-08-30 22:17:58 +0200

    项目的各个头文件

---
+++ Parallel Programming/project/project files/ompbh.cpp
@@ -0,0 +1,14 @@
+//
+// Created by shubham on 08.06.21.
+//
+
+#include "ompbh.h"
+#include "constant.h"
+
+namespace OMP{
+    ///// Implementation of helper function/class for solve here
+}
+
+void OMP::Solve(Body *bodies, const int numBodies, const int numSteps) {
+    // Implementation here
+}
---
+++ Parallel Programming/project/project files/ompbh.h
@@ -0,0 +1,13 @@
+//
+// Created by shubham on 08.06.21.
+//
+
+#ifndef TEST_OMPBH_H
+#define TEST_OMPBH_H
+
+#include "constant.h"
+namespace OMP{
+    void Solve(Body* bodies, const int numBodies, const int numSteps);
+}
+
+#endif //TEST_OMPBH_H
